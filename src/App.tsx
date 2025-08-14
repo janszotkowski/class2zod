c4a22fe1b42ffd217754724e9a150d5f9c48dddd
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-08-14 19:46:52 +0200

    Refactor: Rename "content" to "main" and update styles accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 
 export const App: React.FC = (): React.ReactElement => (
-    <div className={'content'}>
+    <main className={'main'}>
         <div>React, TypeScript, ESLint, Stylelint & RSBuild template.</div>
-    </div>
+    </main>
 );
