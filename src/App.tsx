51f461c2d686d1197f2578508136c789233feef3
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-08-14 14:47:41 +0200

    Initialize project with React, TypeScript, ESLint, Stylelint, and Rsbuild configuration

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './App.css';
+
+export const App: React.FC = (): React.ReactElement => (
+    <div className={'content'}>
+        <div>React, TypeScript, ESLint, Stylelint & RSBuild template.</div>
+    </div>
+);
